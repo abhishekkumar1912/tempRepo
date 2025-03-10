20339ff722c724d4f8c907cbbf46c5e9370885b3
abhishek kumar <as2952352@gmail.com>
2025-03-10 20:28:56 +0530

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import org.example.sample.Helper;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        Helper.printMessage();
+    }
+}
---
+++ src/main/java/org/example/sample/Helper.java
@@ -0,0 +1,9 @@
+package org.example.sample;
+
+
+public class Helper {
+     public static void printMessage() {
+        System.out.println("message from helper package");
+
+    }
+}
